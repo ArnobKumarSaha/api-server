842edb92db22ddba3ad284f56195e27119e8721f
Arnob kumar saha <arnob@appscode.com>
2022-11-04 17:53:26 +0600

    Api server pushed
    Signed-off-by: Arnob kumar saha <arnob@appscode.com>

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/Arnobkumarsaha/new-server
+
+go 1.18
+
+require (
+	github.com/go-chi/chi/v5 v5.0.7
+	github.com/go-chi/render v1.0.2
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+)
+
+require github.com/ajg/form v1.5.1 // indirect
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/Arnobkumarsaha/new-server/auth"
+	"github.com/Arnobkumarsaha/new-server/controllers"
+	"github.com/Arnobkumarsaha/new-server/schemas"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"log"
+	"net/http"
+)
+
+func main() {
+	schemas.AddRequiredData()
+	port := "8080"
+	log.Printf("Starting up on http://localhost:%s", port)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	//r.Get("/pp", check)
+
+	r.Post("/signin", auth.SignIn)
+	r.Get("/welcome", auth.Welcome)
+	r.Post("/logout", auth.LogOut)
+
+	/*
+		ControllerProductResource should be initialized if it has a pointer indside.
+		But as AuthProductResource has no fields, we hadn't to do that.
+		For details :: https://play.golang.org/p/x6q-2HUfTH
+	*/
+	r.Mount("/products", (&controllers.ControllerProductResource{}).Routes())
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
